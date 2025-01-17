77726b43cb6b81024421f5f22008a10e17ee5d29
FrancescoCarlini <francescocarlini36@gmail.com>
2025-01-17 10:29:11 +0100

    aggiunti componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrazioneComponent } from './components/registrazione/registrazione.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, LoginComponent, RegistrazioneComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule],
   providers: [
     provideAnimationsAsync(),
     providePrimeNG({
